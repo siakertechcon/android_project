1f5b115eb094f6893b72aed53ef33094364046cd
siakertechcon <simon.akers@techconsulting.net>
2025-02-13 14:08:16 -0500

    Make Take

--- app/src/main/java/com/example/viewmodelproject/ui/ProductCard.kt
+++ app/src/main/java/com/example/viewmodelproject/ui/ProductCard.kt
@@ -1,18 +1,34 @@
 package com.example.viewmodelproject.ui
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import coil3.compose.AsyncImage
 import com.example.viewmodelproject.model.Product
 
 @Composable
 fun ProductCard(product: Product) {
-    Text(text="Name: ${product.name}")
-    Text(text="Price: ${product.price}")
-    Text(text="Description: ${product.description}")
-    Text(text="Rating: ${product.rating}")
-    AsyncImage(
-        model = product.imageUrl,
-        contentDescription = "Translated description of what the image contains"
-    )
+    val context = LocalContext.current
+    Box(
+        Modifier.clickable(
+        onClick = {
+            Toast.makeText(
+                context,
+                "Rating: ${product.rating}, Price: ${product.price}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    )) {
+        Text(text="Name: ${product.name}")
+        Text(text="Description: ${product.description}")
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = "Translated description of what the image contains"
+        )
+    }
 }
