63523ba041745038f87eb9f680289e717b31e4e4
siakertechcon <simon.akers@techconsulting.net>
2025-02-13 14:13:33 -0500

    Remove indication

--- app/src/main/java/com/example/viewmodelproject/ui/ProductCard.kt
+++ app/src/main/java/com/example/viewmodelproject/ui/ProductCard.kt
@@ -2,9 +2,11 @@ package com.example.viewmodelproject.ui
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -13,17 +15,20 @@ import com.example.viewmodelproject.model.Product
 
 @Composable
 fun ProductCard(product: Product) {
+    val interactionSource = remember { MutableInteractionSource() }
     val context = LocalContext.current
     Box(
         Modifier.clickable(
-        onClick = {
-            Toast.makeText(
-                context,
-                "Rating: ${product.rating}, Price: ${product.price}",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    )) {
+            interactionSource = interactionSource,
+            indication = null,
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Rating: ${product.rating}, Price: ${product.price}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        )) {
         Text(text="Name: ${product.name}")
         Text(text="Description: ${product.description}")
         AsyncImage(
