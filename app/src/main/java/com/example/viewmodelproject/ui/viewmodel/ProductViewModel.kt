54710acda31a12dad510224209ad9feb7bf305b0
siakertechcon <simon.akers@techconsulting.net>
2025-02-13 15:40:14 -0500

    Suspend Function

--- app/src/main/java/com/example/viewmodelproject/ui/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/viewmodelproject/ui/viewmodel/ProductViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.viewmodelproject.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.viewmodelproject.model.Product
 import com.example.viewmodelproject.model.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
-object ProductViewModel {
-    fun get(): List<Product> {
-        return ProductRepository.getProducts()
+
+object ProductViewModel: ViewModel() {
+    suspend fun get(): List<Product> {
+        val products = viewModelScope.async {
+            ProductRepository.getProducts()
+        }
+        return products.await()
     }
 }
